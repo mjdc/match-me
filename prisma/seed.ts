1c7365c5a091054aba62355bb9037df0c580ab23
mjdc <meljohndc@gmail.com>
2025-09-22 21:36:34 +0400

    upsert instead of create

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,15 +6,41 @@ const prisma = new PrismaClient();
 
 async function seedMembers() {
   await Promise.all(
-    membersData.map(async member => {
-      return prisma.user.create({
-        data: {
+    membersData.map(async (member) => {
+      await prisma.user.upsert({
+        where: { email: member.email },
+        update: {
+          name: member.name,
+          image: member.image,
+          profileComplete: true,
+          passwordHash: await hash('password', 10),
+          member: {
+            update: {
+              dateOfBirth: new Date(member.dateOfBirth),
+              gender: member.gender,
+              name: member.name,
+              updated: new Date(member.lastActive),
+              description: member.description,
+              city: member.city,
+              country: member.country,
+              image: member.image,
+              photos: {
+                upsert: {
+                  where: { url: member.image },
+                  update: { isApproved: true },
+                  create: { url: member.image, isApproved: true },
+                },
+              },
+            },
+          },
+        },
+        create: {
           email: member.email,
           emailVerified: new Date(),
           name: member.name,
-          passwordHash: await hash('password', 10),
           image: member.image,
           profileComplete: true,
+          passwordHash: await hash('password', 10),
           member: {
             create: {
               dateOfBirth: new Date(member.dateOfBirth),
@@ -29,38 +55,46 @@ async function seedMembers() {
               photos: {
                 create: {
                   url: member.image,
-                  isApproved: true
-                }
-              }
-            }
-          }
-        }
+                  isApproved: true,
+                },
+              },
+            },
+          },
+        },
       });
     })
   );
 }
 
-
 async function seedAdmin() {
-    return prisma.user.create({
-        data: {
-            email: 'admin@test.com',
-            emailVerified: new Date(),
-            name: 'Admin',
-            passwordHash: await hash('password', 10),
-            role: 'ADMIN'
-        }
-    })
+  await prisma.user.upsert({
+    where: { email: 'admin@test.com' },
+    update: {
+      name: 'Admin',
+      role: 'ADMIN',
+      passwordHash: await hash('password', 10),
+      emailVerified: new Date(),
+    },
+    create: {
+      email: 'admin@test.com',
+      emailVerified: new Date(),
+      name: 'Admin',
+      passwordHash: await hash('password', 10),
+      role: 'ADMIN',
+    },
+  });
 }
 
 async function main() {
-    await seedMembers();
-    await seedAdmin();
+  await seedMembers();
+  await seedAdmin();
 }
 
-main().catch(e => {
+main()
+  .catch((e) => {
     console.error(e);
     process.exit(1);
-}).finally(async () => {
+  })
+  .finally(async () => {
     await prisma.$disconnect();
-})
+  });
