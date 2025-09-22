b04eeef2d3e18fd6975c5dc7e964faba1f798b80
mjdc <meljohndc@gmail.com>
2025-09-22 19:02:06 +0400

    updated seed command

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,36 +5,41 @@ import { hash } from 'bcryptjs';
 const prisma = new PrismaClient();
 
 async function seedMembers() {
-    return membersData.map(async member => prisma.user.create({
+  await Promise.all(
+    membersData.map(async member => {
+      return prisma.user.create({
         data: {
-            email: member.email,
-            emailVerified: new Date(),
-            name: member.name,
-            passwordHash: await hash('password', 10),
-            image: member.image,
-            profileComplete: true,
-            member: {
+          email: member.email,
+          emailVerified: new Date(),
+          name: member.name,
+          passwordHash: await hash('password', 10),
+          image: member.image,
+          profileComplete: true,
+          member: {
+            create: {
+              dateOfBirth: new Date(member.dateOfBirth),
+              gender: member.gender,
+              name: member.name,
+              created: new Date(member.created),
+              updated: new Date(member.lastActive),
+              description: member.description,
+              city: member.city,
+              country: member.country,
+              image: member.image,
+              photos: {
                 create: {
-                    dateOfBirth: new Date(member.dateOfBirth),
-                    gender: member.gender,
-                    name: member.name,
-                    created: new Date(member.created),
-                    updated: new Date(member.lastActive),
-                    description: member.description,
-                    city: member.city,
-                    country: member.country,
-                    image: member.image,
-                    photos: {
-                        create: {
-                            url: member.image,
-                            isApproved: true
-                        }
-                    }
+                  url: member.image,
+                  isApproved: true
                 }
+              }
             }
+          }
         }
-    }))
+      });
+    })
+  );
 }
+
 
 async function seedAdmin() {
     return prisma.user.create({
