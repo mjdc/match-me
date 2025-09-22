80f936234f2e2017be92b83f6f096fbb8c1f0584
mjdc <meljohndc@gmail.com>
2025-09-22 22:33:11 +0400

    added google to images domains

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['res.cloudinary.com', 'avatars.githubusercontent.com'],
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
   },
 };
 
